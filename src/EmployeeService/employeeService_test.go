0bcd3b9a43fb12d849bbd6582d843c840bf29773
Jayd <jaydpather@gmail.com>
2020-10-11 13:14:49 -0500

    first UT passing

--- src/EmployeeService/employeeService_test.go
+++ src/EmployeeService/employeeService_test.go
@@ -3,27 +3,37 @@ package employeeService
 import(
 	"fmt"
 	"testing"
-	//"gomock"
-	//"mock_employeeRepository"
+	"github.com/golang/mock/gomock"
+	"GoMockFirstTry/src/mock_employeeRepository"
 )
+
+func formatErrMsg_WrongReturnValue(expected int, actual int) string {
+	return fmt.Sprintf("expected %d, but got %d", expected, actual)
+}
 
 func TestShouldReturnZeroIfWorking(t *testing.T) {
 	actual := ShouldReturnZeroIfWorking()
 	expected := 0
 
-	if(actual != expected){
-		t.Error(fmt.Sprintf("expected %d, but got %d", expected, actual))
+	if(expected != actual){
+		t.Error(formatErrMsg_WrongReturnValue(expected, actual))
 	}
-	
 }
 
-// func TestInsertEmployee(t *testing.T) {
-// 	mockController = gomock.NewController(t)
-// 	defer mockController.Finish() //this calls our asserts at the end of the function
+func TestInsertEmployee(t *testing.T) {
+	mockController := gomock.NewController(t)
+	defer mockController.Finish() //this calls our asserts at the end of the function
 
-// 	mockRepo := NewMockIEmployeeRepository(mockController)
+	mockRepo := mock_employeeRepository.NewMockIEmployeeRepository(mockController)
 
-// 	mockRepo.EXPECT().InsertEmployee().Return(1)
-// }
+	expectedValue:= 1
+	mockRepo.EXPECT().InsertEmployee().Return(expectedValue)
+
+	actualValue := InsertEmployee(mockRepo)
+	if(expectedValue != actualValue) {
+		t.Error(formatErrMsg_WrongReturnValue(expectedValue, actualValue))
+	}
+
+}
 
 
